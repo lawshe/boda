a212cfccad315c725f3de1fdc0e34af594a67845
lawshe <jessica@jlawshe.com>
2016-08-09 10:44:36 -0500

    Variables - margins, button colors, and constants for colors

--- config/variables.js
+++ config/variables.js
@@ -1,10 +1,17 @@
+const blue = '#004B99';
+const green = '#00994e';
+
 module.exports = {
-  $black: '#323231',
-  $green: '#015432',
-  $greenBright: '#02a260',
-  $white: '#fdfef5',
-  $blue: '#0F52BA',
+  $black: '#001a0d',
+  $green: '#006634',
+  $greenBright: green,
+  $white: '#fbf9f7',
+  $blue: blue,
   $footerHeight: '200px',
   $linkTransition: 'all 250ms ease',
-  $linkTransitionSpeed: '250ms'
+  $linkTransitionSpeed: '250ms',
+  $marginMd: '20px',
+  $marginLg: '20px',
+  $btnSubmit: blue,
+  $btnAction: green
 };
