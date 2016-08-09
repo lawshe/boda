0bedad5ae73833ab11299f8e16541f48defd70a7
lawshe <jessica@jlawshe.com>
2016-08-09 13:55:28 -0500

    Visitor header - removed menu
    Menu now in visitor layout template

--- source/client/components/visitor/layout/main-header.js
+++ source/client/components/visitor/layout/main-header.js
@@ -6,7 +6,6 @@ import type from 'styles/type';
 import local from './_styles';
 
 import 'styles/type.css';
-import Menu from './menu';
 import borderImage from '../../../../../static/images/borders/leafs.png';
 import PrettyDate from '../../_partials/pretty-date.js';
 
@@ -22,7 +21,6 @@ import PrettyDate from '../../_partials/pretty-date.js';
 export default () => (
   <div className={`${local.pageHeader} page-header`}>
     <h1 className={`${type.gothic} ${glob.center}`}><Link to="/"><PrettyDate /></Link></h1>
-    <Menu />
     <img alt="border fern" src={borderImage} />
   </div>
 );
