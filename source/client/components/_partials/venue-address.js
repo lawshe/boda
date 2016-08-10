e4acfa519d5b0e8aa18934561b3552968ffdfef2
lawshe <jessica@jlawshe.com>
2016-08-10 10:36:35 -0500

    Partial - Venue address

---
+++ source/client/components/_partials/venue-address.js
@@ -0,0 +1,3 @@
+import wedding from '../../../../config/wedding.js';
+
+export default `${wedding.venue.address.street} ${wedding.venue.address.city}, ${wedding.venue.address.state} ${wedding.venue.address.zip}`
