b22d0ec8e25bd0595eafe64d64bd76a0a8d493a7
lawshe <jessica@jlawshe.com>
2016-08-10 10:26:12 -0500

    Google map style - separate file

---
+++ source/client/components/visitor/details/map-style.js
@@ -0,0 +1,114 @@
+import variables from '../../../../../config/variables.js';
+
+module.exports = [
+  {
+    featureType: 'administrative.neighborhood',
+    stylers: [
+      { visibility: 'off' }
+    ]
+  },
+  {
+    featureType: 'landscape.man_made',
+    elementType: 'geometry',
+    stylers: [
+      { color: variables.$white }
+    ]
+  },
+  {
+    featureType: 'landscape.natural',
+    elementType: 'geometry',
+    stylers: [
+      { color: variables.$greenMuted }
+    ]
+  },
+  {
+    featureType: 'landscape.natural',
+    elementType: 'labels.text',
+    stylers: [
+      { color: variables.$greenMuted }
+    ]
+  },
+  {
+    featureType: 'poi',
+    elementType: 'geometry',
+    stylers: [
+      { visibility: 'simplified' },
+      { color: variables.$greenMuted }
+    ]
+  },
+  {
+    featureType: 'poi.government',
+    elementType: 'geometry',
+    stylers: [
+      { color: variables.$white }
+    ]
+  },
+  {
+    featureType: 'poi.school',
+    elementType: 'geometry',
+    stylers: [
+      { color: variables.$white }
+    ]
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'geometry',
+    stylers: [
+      { color : variables.$gray },
+      { visibility: 'simplified' }
+    ]
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'labels.text',
+    stylers: [
+      { color : variables.$grayDark },
+      { visibility: 'simplified' }
+    ]
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry',
+    stylers: [
+      { color : variables.$gray },
+      { visibility: 'simplified' }
+    ]
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'labels.text',
+    stylers: [
+      { color : variables.$grayDark },
+      { visibility: 'simplified' }
+    ]
+  },
+  {
+    featureType: 'road.local',
+    elementType: 'geometry',
+    stylers: [
+      { color : variables.$grayLight },
+      { visibility: 'simplified' }
+    ]
+  },
+  {
+    featureType: 'road.local',
+    elementType: 'labels',
+    stylers: [
+      { visibility: 'off' }
+    ]
+  },
+  {
+    featureType: 'water',
+    elementType: 'geometry',
+    stylers: [
+      { color: variables.$blueLight }
+    ]
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels',
+    stylers: [
+      { color: variables.$blue }
+    ]
+  },
+];
