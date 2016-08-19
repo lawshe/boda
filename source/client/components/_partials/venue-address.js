b303e79e1dab7a0b2c686a7ba01bf778f2e35375
lawshe <jessica@jlawshe.com>
2016-08-19 11:17:01 -0500

    New Partial - Venue address

---
+++ source/client/components/_partials/venue-address.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import wedding from '../../../../config/wedding.js';
+
+
+/**
+  *
+  * Partial - pretty venue address
+  *
+  * @param none
+  *
+  * @return react component
+  */
+
+export default () => {
+  const address = wedding.venue.address;
+  return (
+    <span>
+      {address.street}
+      <br />
+      {address.city}, {address.state} {address.zip}
+    </span>
+  );
+};
