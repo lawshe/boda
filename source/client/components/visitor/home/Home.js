ebd4740c6c36a3bf83c1709931118b2dc24c13fb
lawshe <jessica@jlawshe.com>
2016-10-11 15:07:58 -0500

    Removed unused

--- source/client/components/visitor/home/Home.js
+++ source/client/components/visitor/home/Home.js
@@ -1,10 +1,6 @@
 import React from 'react';
-
 import wedding from '../../../../../config/wedding.js';
-
-import 'static/vendor/font-awesome/css/font-awesome.min.css';
 import glob from 'styles/app';
-import { Row, Col } from 'react-bootstrap';
 import local from './_styles';
 import EucThree from '../../svg/euc-3';
 import EucFour from '../../svg/euc-4';
@@ -29,7 +25,9 @@ const Home = () => {
           <div className={`${local.euc} ${local.eucTop}`}>
             <EucThree color="$green" />
           </div>
-          <h1 className={`${local.header}`}>{coupleOne}<br />& {coupleTwo}</h1>
+          <h1 className={`${local.header}`}>
+            {coupleOne}<br />& {coupleTwo}
+          </h1>
           <div className={`${local.euc} ${local.eucBtm}`}>
             <EucFour color="$green" />
           </div>
