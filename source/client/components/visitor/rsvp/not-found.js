297fc70b316aa7f8109edb3c7c330c0e627e7683
lawshe <jessica@jlawshe.com>
2016-10-13 14:08:04 -0500

    Not Found RSVP - wrapped in col

--- source/client/components/visitor/rsvp/not-found.js
+++ source/client/components/visitor/rsvp/not-found.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import glob from 'styles/app';
-
+import { Col } from 'react-bootstrap';
 import wedding from '../../../../../config/wedding';
 
 /**
@@ -15,10 +15,14 @@ import wedding from '../../../../../config/wedding';
 const supportEmail = <a href={`mailto:${wedding.email}?Subject=RSVP Not found`}>{wedding.email}</a>;
 
 const notFound = () => (
-  <div className={`${glob.fourOhFour}`}>
-    <h2>Invitation not found</h2>
-    <h4>Email {supportEmail} for support.</h4>
-  </div>
+  <Col xs={10} xsOffset={1} sm={10} smOffset={1} md={8} mdOffset={2} lg={6} lgOffset={3} style={{ textAlign: 'center' }}>
+    <div className={`${glob.card} ${glob.cardImage} ${glob.fourOhFour}`}>
+      <div className={`${glob.cardContent}`}>
+        <h2>Invitation not found</h2>
+        <h4>Email {supportEmail} for support.</h4>
+      </div>
+    </div>
+  </Col>
 );
 
 export default notFound;
