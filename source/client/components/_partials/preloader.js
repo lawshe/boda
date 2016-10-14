0847693743e97ca003031e575a18812ce3201b2d
lawshe <jessica@jlawshe.com>
2016-10-14 11:36:41 -0500

    Loading partial

---
+++ source/client/components/_partials/preloader.js
@@ -0,0 +1,26 @@
+import React from 'react';
+// import wedding from '../../../../config/wedding.js';
+import local from './_styles';
+
+
+/**
+  *
+  * Partial - preloader
+  *
+  * @param none
+  *
+  * @return react component
+  */
+
+
+const images = process.env.NODE_ENV === 'production' ? 'images' : `http://127.0.0.1:9095/static/images`;
+
+export default () => (
+  <div className={local.showbox}>
+    <div className={local.loader}>
+      <svg className={local.circular} viewBox="25 25 50 50">
+      <circle className={local.path} cx="50" cy="50" r="20" fill="none" strokeWidth="2" strokeMiterlimit="10"/>
+    </svg>
+  </div>
+</div>
+);
