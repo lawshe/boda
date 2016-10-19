8fac5933c20caea609ff0e82b8977ef005e9c2e1
lawshe <jessica@jlawshe.com>
2016-10-19 16:28:14 -0500

    Admin invitations - added count for attending and returned

--- source/client/components/admin/invitations/InvitationList.js
+++ source/client/components/admin/invitations/InvitationList.js
@@ -19,41 +19,58 @@ const mapDataToProps = {
   invitations: (hz) => hz('invitations')
 };
 
-const InvitationList = (props) => (
-  <div>
-    <h1>Invitations</h1>
-    <Row>
-      <Col md={12}>
-        <Well>
-          <Table>
-            <thead>
-              <tr>
-                <th>Returned RSVP?</th>
-                <th>Total Attending</th>
-                <th>Guests</th>
-                <th>Short Name</th>
-                <th>Plus Allowed</th>
-                <th>Plus Bringing</th>
-                <th>Admin</th>
-                <th>Visitor</th>
-              </tr>
-            </thead>
-            <tbody>
-            {props.invitations.map(
-              invitation => (
-                <InvitationItem
-                  key={invitation.id}
-                  invitation={invitation}
-                />
-              )
-            )}
-            </tbody>
-          </Table>
-        </Well>
-      </Col>
-    </Row>
-  </div>
-);
+const InvitationList = (props) => {
+  let totalAttending = 0;
+  let totalReturned = 0;
+
+  props.invitations.forEach((i) => {
+    if (i.processed && i.processed.attending) {
+      totalAttending = totalAttending + i.processed.attending;
+    }
+
+    if (i.returned) {
+      totalReturned++;
+    }
+  });
+
+  return (
+    <div>
+      <h1 style={{ marginBottom: '0px' }}>Invitations</h1>
+      <h3>Returned: {totalReturned}</h3>
+      <h3>Attending: {totalAttending}</h3>
+      <Row>
+        <Col md={12}>
+          <Well>
+            <Table>
+              <thead>
+                <tr>
+                  <th>Returned RSVP?</th>
+                  <th>Total Attending</th>
+                  <th>Guests</th>
+                  <th>Short Name</th>
+                  <th>Plus Allowed</th>
+                  <th>Plus Bringing</th>
+                  <th>Admin</th>
+                  <th>Visitor</th>
+                </tr>
+              </thead>
+              <tbody>
+              {props.invitations.map(
+                invitation => (
+                  <InvitationItem
+                    key={invitation.id}
+                    invitation={invitation}
+                  />
+                )
+              )}
+              </tbody>
+            </Table>
+          </Well>
+        </Col>
+      </Row>
+    </div>
+) ;
+};
 
 export default subscribe({
   mapDataToProps
