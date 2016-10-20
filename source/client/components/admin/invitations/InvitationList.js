757b8aa62aa1e103d10f4469fa4e1b8809edd3cc
lawshe <jessica@jlawshe.com>
2016-10-20 12:51:38 -0500

    Admin invitation list -  added total invited count

--- source/client/components/admin/invitations/InvitationList.js
+++ source/client/components/admin/invitations/InvitationList.js
@@ -22,22 +22,39 @@ const mapDataToProps = {
 const InvitationList = (props) => {
   let totalAttending = 0;
   let totalReturned = 0;
+  let totalInvites = 0;
+  let totalInvitedPeople = 0;
 
-  props.invitations.forEach((i) => {
-    if (i.processed && i.processed.attending) {
-      totalAttending = totalAttending + i.processed.attending;
-    }
+  if (props.invitations) {
+    totalInvites = props.invitations.length;
 
-    if (i.returned) {
-      totalReturned++;
-    }
-  });
+    props.invitations.forEach((i) => {
+      // Attending
+      if (i.processed && i.processed.attending) {
+        totalAttending = totalAttending + i.processed.attending;
+      }
+
+      // Returned RSVPs
+      if (i.returned) {
+        totalReturned++;
+      }
+
+      // Total Invited
+      if (i.guests) {
+        totalInvitedPeople = totalInvitedPeople + i.guests.length;
+      }
+
+      if (i.plus && i.plus.allowed) {
+        totalInvitedPeople = totalInvitedPeople + i.plus.allowed;
+      }
+    });
+  }
 
   return (
     <div>
       <h1 style={{ marginBottom: '0px' }}>Invitations</h1>
-      <h3>Returned: {totalReturned}</h3>
-      <h3>Attending: {totalAttending}</h3>
+      <h3><u>Returned RSVPs</u>: {totalReturned} of {totalInvites}</h3>
+      <h3><u>Total Attending</u>: {totalAttending} of {totalInvitedPeople}</h3>
       <Row>
         <Col md={12}>
           <Well>
